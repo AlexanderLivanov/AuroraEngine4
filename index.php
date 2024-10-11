5965c276b4629f7f7f926dc6a5d77f2e910d5ea1
AlexanderLivanov <help.underlinegroup@gmail.com>
2024-10-11 22:11:39 +0300

    init

---
+++ index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Stayinspired</title>
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Playwrite+HR+Lijeva:wght@100..400&display=swap" rel="stylesheet">
+</head>
+<body>
+  <header>
+    <div class="logo-field">
+      Stayinspired
+    </div>
+    <div class="icon-field">
+      β
+    </div>
+    <div class="search-field">
+      <input type="text" placeholder="> Ищете кого-то?">
+    </div>
+    <div class="search-button">
+      <input type="button" name="search-button" id="search-button" value="Найти">
+    </div>
+  </header>
+  <div class="main-container" style="height: 10000px;">
+    <div class="fullscreen-post-container">
+      <div class="main-post-container">
+        <div class="text-post-container">
+          <div class="meta-post-container">
+            <img src="" alt="" class="avatar">
+            <p class="post-author"></p>
+            <p class="post-author-rating"></p>
+          </div>
+          
+          <div class="text-post-container">
+            <div class="post-title">
+
+            </div>
+            <div class="post-text">
+
+            </div>
+            <div class="post-info">
+              <p class="post-date">
+
+              </p>
+              <p class="post-views">
+
+              </p>
+            </div>
+          </div>
+
+          <div class="post-rating">
+            <div class="post-plus">
+
+            </div>
+            <div class="post-minus">
+
+            </div>
+            <div class="post-comments">
+
+            </div>
+          </div>
+        </div>
+        <div class="gallery-post-container">
+          <div class="gallery-image">
+            <img src="" alt="" class="gallery-image-item">
+          </div>
+          <div class="previous-gallery-item-button">
+            
+          </div>
+          <div class="next-gallery-item-button">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="bottom_panel">
+    <div class="bottom-panel-button">
+      <a href="" class="icon-button" title="Главная">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home-2" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+          <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
+          <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
+          <path d="M10 12h4v4h-4z" />
+        </svg>
+      </a>
+    </div>
+    <div class="bottom-panel-button">
+      <a href="" class="icon-button" title="TwinFeed">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-layers-linked" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+          <path d="M19 8.268a2 2 0 0 1 1 1.732v8a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2v-8a2 2 0 0 1 2 -2h3" />
+          <path d="M5 15.734a2 2 0 0 1 -1 -1.734v-8a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-3" />
+        </svg>
+      </a>
+    </div>
+    <div class="bottom-panel-button">
+      <a href="" class="icon-button" title="Новости">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-news" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+          <path d="M16 6h3a1 1 0 0 1 1 1v11a2 2 0 0 1 -4 0v-13a1 1 0 0 0 -1 -1h-10a1 1 0 0 0 -1 1v12a3 3 0 0 0 3 3h11" />
+          <path d="M8 8l4 0" />
+          <path d="M8 12l4 0" />
+          <path d="M8 16l4 0" />
+        </svg>
+      </a>
+    </div>
+    <div class="bottom-panel-button">
+      <a href="" class="icon-button" title="Профиль">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+          <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+          <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+          <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
+        </svg>
+      </a>
+    </div>
+  </div>
+  <script>
+    let lastScrollTop = 0;
+    const header = document.querySelector('header');
+    const bottom_panel = document.querySelector('.bottom_panel');
+
+    window.addEventListener('scroll', function() {
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollTop > lastScrollTop) {
+            header.style.transform = 'translateY(-100%)'; 
+            bottom_panel.style.transform = 'translateY(+100%)';
+        } else {
+            header.style.transform = 'translateY(0)';
+            bottom_panel.style.transform = 'translateY(0)';
+        }
+        lastScrollTop = scrollTop;
+    });
+
+  </script>
+</body>
+</html>
